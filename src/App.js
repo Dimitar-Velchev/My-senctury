d530c0530e5e22735abb7ecf86bcbde229625af4
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-11-15 18:19:33 +0000

    change css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomePage from "./components/HomePage";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <HomePage/>
+      <Home/>
       <Footer/>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-function HomePage () {
+function Home () {
     return (
 <div id="body">
   <div className="banner">&nbsp;</div>
@@ -58,15 +58,15 @@ function HomePage () {
       </div>
     </div>
   </div>
-  <div className="featured">
+  <div class="featured">
     <ul>
       <li><a href="#"><img src="images/organic-and-chemical-free.jpg" width="300" height="90" alt=""/></a></li>
       <li><a href="#"><img src="images/good-food.jpg" width="300" height="90" alt=""/></a></li>
-      <li className="last"><a href="#"><img src="images/pet-grooming.jpg" width="300" height="90" alt=""/></a></li>
+      <li class="last"><a href="#"><img src="images/pet-grooming.jpg" width="300" height="90" alt=""/></a></li>
     </ul>
   </div>
 </div>
 );
 }
 
-export default HomePage
+export default Home
