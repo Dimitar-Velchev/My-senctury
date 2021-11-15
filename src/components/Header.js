c6c994f1b6b789ba8aa12554e26d959a245b1dec
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-11-15 17:57:36 +0000

    Ineceliaze header and navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ return (
 <div id="header"> <a href="#" id="logo"><img src="images/logo.gif" width="310" height="114" alt=""/></a>
     <ul class="navigation">
       <li class="active"><a href="index.html">Home</a></li>
-      <li><a href="petmart.html">PetMart</a></li>
-      <li><a href="about.html">About us</a></li>
-      <li><a href="blog.html">Blog</a></li>
-      <li><a href="petguide.html">PetGuide</a></li>
-      <li><a href="contact.html">Contact us</a></li>
+      <li><a href="petmart.html">Catalog</a></li>
+      <li><a href="about.html">Register</a></li>
+      <li><a href="blog.html">Login</a></li>
+      <li><a href="petguide.html">My shop</a></li>
+      <li><a href="contact.html">Logout</a></li>
     </ul>
 </div>
 );
