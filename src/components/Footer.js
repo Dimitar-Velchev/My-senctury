a23d0a247dfb5448b75362366ac9e5093d693daf
Dimitar <dimitarvelchev59@gmail.com>
2021-11-15 20:36:34 +0200

    footer upgrade

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,46 @@
 function Footer() {
   return (
-
-  <div id="footer">
-    <div id="footnote">
-      <div class="section">
-        Copyright &copy; 2012 <a href="#">Company Name</a> All rights reserved |
-        Website Template By{" "}
-        <a target="_blank" href="http://www.freewebsitetemplates.com/">
-          freewebsitetemplates.com
-        </a>
+    <div id="footer">
+      <div class="featured">
+        <ul>
+          <li>
+            <a href="#">
+              <img
+                src="images/organic-and-chemical-free.jpg"
+                width="300"
+                height="90"
+                alt=""
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="images/good-food.jpg" width="300" height="90" alt="" />
+            </a>
+          </li>
+          <li class="last">
+            <a href="#">
+              <img
+                src="images/pet-grooming.jpg"
+                width="300"
+                height="90"
+                alt=""
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div id="footnote">
+        <div class="section">
+          Copyright &copy; 2012 <a href="#">Company Name</a> All rights reserved
+          | Website Template By{" "}
+          <a target="_blank" href="http://www.freewebsitetemplates.com/">
+            freewebsitetemplates.com
+          </a>
+        </div>
       </div>
     </div>
-    </div>
-
   );
 }
 
-export default Footer
+export default Footer;
