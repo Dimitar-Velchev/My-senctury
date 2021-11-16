2dc17658543bfb17e09164a0fb8fe55975de3c91
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-11-16 21:20:35 +0000

    d

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,9 @@ import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
