81c6a06e9d1b7f2ae8cd56a4deac3b96769fad68
Dimitar <dimitarvelchev59@gmail.com>
2021-11-26 13:16:57 +0200

    Update Pet Model on server

--- server/models/Pet.js
+++ server/models/Pet.js
@@ -1,13 +1,17 @@
 const { Schema, model } = require("mongoose");
 
 const schema = new Schema({
-  name: { type: String, required: [true, 'Name is required'] },
-  age: { type: Number, required: [true, 'Age is required'] },
-  sex: { type: String, required: [true, 'Sex is required'] },
-  img: { type: String, required: [true, 'Image is required'] },
-  // category: { type: String, required:[true, 'Category is required'],  enum: ['house', 'apartment', 'room', 'other'] },
-  // description: { type: String },
-  // owner: { type: Schema.Types.ObjectId, ref: "User" },
+  name: { type: String, required: [true, "Name is required"] },
+  age: { type: Number, required: [true, "Age is required"] },
+  gender: { type: String, required: [true, "Gender is required"] },
+  img: { type: String, required: [true, "Image is required"] },
+  description: { type: String },
+  category: {
+    type: String,
+    required: [true, "Category is required"],
+    enum: ["dog", "cat"],
+  },
+  owner: { type: Schema.Types.ObjectId, ref: "User" },
   // reviews: [{type: Schema.Types.ObjectId, ref: "Review" }]
 });
 
