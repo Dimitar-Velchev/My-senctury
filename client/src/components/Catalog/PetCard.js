9844120938e9954157954eb2c3041cd5c3282dcf
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-11-30 22:13:29 +0000

    add view all button

--- client/src/components/Catalog/PetCard.js
+++ client/src/components/Catalog/PetCard.js
@@ -22,3 +22,24 @@ function PetCard() {
   
   export default PetCard;
   
+
+import { Card, Button, Image } from "react-bootstrap";
+
+function PetCard() {
+  return (
+    <Card style={{ width: "22rem" }}>
+      <Card.Img variant="top" src="./images/reg-form-puppy.jpg" />
+      <Card.Body>
+        <Card.Title>Lucky</Card.Title>
+        <Card.Text>
+          <Image src="./images/hourglass-icon.svg" roundedCircle />
+          Age: 3 <br />
+          <Image src="./images/gender-icon.svg" roundedCircle /> Gender: Male
+        </Card.Text>
+        <Button variant="warning">Learn more about...</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default PetCard;
--- client/src/components/Home/PetCategory.js
+++ client/src/components/Home/PetCategory.js
@@ -1,18 +1,17 @@
-function PetCategory({pet}) {
+import { NavLink } from "react-router-dom";
+
+function PetCategory({ pet }) {
   return (
     <li>
-      <a href='#'>
-        <img
-          src={pet.src}
-          width="114"
-          height="160"
-          alt=""
-        />
+      <a href="#">
+        <img src={pet.src} width="114" height="160" alt="" />
       </a>
       <h2>{pet.title}</h2>
       <p>
-       {pet.text}
-      <a className="more" href="#">View all</a>
+        {pet.text}
+        <NavLink className="more" to="/catalog">
+          View all
+        </NavLink>
       </p>
     </li>
   );
