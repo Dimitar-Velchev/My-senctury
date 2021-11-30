76f8e5dada0a4a8b8136359ebc2a2644e269d36c
Dimitar <dimitarvelchev59@gmail.com>
2021-11-30 18:52:38 +0200

    add styles to details page

--- client/src/components/Details/PetDetails.js
+++ client/src/components/Details/PetDetails.js
@@ -1,14 +1,15 @@
 import { Container, Row, Col, Image, Button } from "react-bootstrap";
+import './PetDetails.css';
 
 function Details() {
   return (
     <Container style={{ minHeight: "90vh" }}>
       <Row>
         <Col>
-          <Image src="/images/bunny.jfif" thumbnail></Image>
+          <Image src="/images/bunny.jfif" thumbnail className="img-left"></Image>
         </Col>
         <Col>
-          <div>
+          <div className="pet-info">
             <h5>Name: Lucky </h5>
             <h5>Age: 3</h5>
             <h5>Gender: Male</h5>
@@ -25,7 +26,7 @@ function Details() {
               Sprayed/Neutered: <Image src="/images/check-icon.svg" />{" "}
               <Image src="/images/cancel-icon.svg" />{" "}
             </h5>
-            <h5>Owner email:mxb.kativa@gmail.com </h5>
+            <h5>Owner email: mxb.kativa@gmail.com </h5>
             <Button variant="outline-warning">Ask about me</Button>{" "}
             <Button variant="success">Update Info</Button>{" "}
             <Button variant="danger">Delete Pet</Button>
