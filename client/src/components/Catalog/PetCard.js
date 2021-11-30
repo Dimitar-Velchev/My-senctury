d0decf5369e2b81673dbe75c5060aab998a02dc6
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-11-30 22:37:51 +0000

    ....

--- client/src/components/Catalog/PetCard.js
+++ client/src/components/Catalog/PetCard.js
@@ -1,34 +1,10 @@
-import {Card, Button, Image} from 'react-bootstrap';
-import {Link} from 'react-router-dom';
-
-function PetCard() {
-    return (
-        <Card>
-        <Card.Img variant="top" src="./images/bunny.jfif" />
-        <Card.Body>
-          <Card.Title>Lucky</Card.Title> 
-          <Card.Text>
-          <Image src="./images/hourglass-icon.svg" roundedCircle />
-            Age: 3 <br />
-            <Image src="./images/gender-icon.svg" roundedCircle /> Gender: Male
-          </Card.Text>
-          <Link to={`/catalog/details/:id`}>
-          <Button variant="warning">Learn more about me</Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    );
-  }
-  
-  export default PetCard;
-  
-
 import { Card, Button, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function PetCard() {
   return (
-    <Card style={{ width: "22rem" }}>
-      <Card.Img variant="top" src="./images/reg-form-puppy.jpg" />
+    <Card>
+      <Card.Img variant="top" src="./images/bunny.jfif" />
       <Card.Body>
         <Card.Title>Lucky</Card.Title>
         <Card.Text>
@@ -36,10 +12,11 @@ function PetCard() {
           Age: 3 <br />
           <Image src="./images/gender-icon.svg" roundedCircle /> Gender: Male
         </Card.Text>
-        <Button variant="warning">Learn more about...</Button>
+        <Link to={`/catalog/details/:id`}>
+          <Button variant="warning">Learn more about me</Button>
+        </Link>
       </Card.Body>
     </Card>
   );
 }
-
 export default PetCard;
