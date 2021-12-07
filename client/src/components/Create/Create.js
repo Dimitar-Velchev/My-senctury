d94eca5bc50dc3ca10451afd9ba253eab73b457f
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-12-07 17:08:35 +0000

    create form

--- client/src/components/Create/Create.js
+++ client/src/components/Create/Create.js
@@ -2,54 +2,72 @@ import { Form, Button } from "react-bootstrap";
 import "./Create.css";
 
 function Create() {
+  function createHandler(e) {
+    e.preventDefault();
+    let formData = new FormData(e.currentTarget);
+    let name = formData.get("name");
+    let age = formData.get("age");
+    let imgUrl = formData.get("imgUrl");
+    let description = formData.get("description");
+    let gender = formData.get("gender");
+    let category = formData.get("category");
+    let neutered = formData.get("neutered");
+
+    //console.log(name,age,imgUrl,description,gender,category,neutered);
+  }
   return (
-          <div className="create-form">
-            <Form>
-              <h1>ADD A PET FORM</h1>
-              <br />
-              <Form.Group className="mb-3">
-                <Form.Control type="text" placeholder="Enter pet name" />
-              </Form.Group>
+    <div classgender="create-form">
+      <Form method="POST" onSubmit={createHandler}>
+        <h1>ADD A PET FORM</h1>
+        <br />
+        <Form.Group className="mb-3">
+          <Form.Control type="text" placeholder="Enter pet name" name="name" />
+        </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Control type="number" placeholder="Enter pet age" />
-              </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control type="number" placeholder="Enter pet age" name="age" />
+        </Form.Group>
 
-              <Form.Group className="mb-3">
-                <Form.Control type="text" placeholder="Enter img URL" />
-              </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Control type="text" placeholder="Enter img URL" name="imgUrl" />
+        </Form.Group>
 
-              <Form.Group
-                className="mb-3"
-                controlId="exampleForm.ControlTextarea1"
-              >
-                <Form.Control as="textarea" rows={3} placeholder="Please enter a brief description" />
-              </Form.Group>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+          <Form.Control
+            as="textarea"
+            rows={3}
+            placeholder="Please enter a brief description"
+            name="description"
+          />
+        </Form.Group>
 
-              <Form.Select aria-label="Default select example">
-                <option>Please select a pet gender</option>
-                <option value="1">Male</option>
-                <option value="2">Female</option>
-              </Form.Select>
-              <br />
+        <Form.Select aria-label="Default select example" name="gender">
+          <option>Please select a pet gender</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </Form.Select>
+        <br />
 
-              <Form.Select aria-label="Default select example">
-                <option>Please select a pet category</option>
-                <option value="1">Cat</option>
-                <option value="2">Dog</option>
-              </Form.Select>
-              <br />
+        <Form.Select aria-label="Default select example" name="category">
+          <option>Please select a pet category</option>
+          <option value="cat">Cat</option>
+          <option value="dog">Dog</option>
+        </Form.Select>
+        <br />
 
-              <Form.Group className="mb-3">
-                <Form.Check type="checkbox" label="Check if the pet is neutered/sprayed." />
-              </Form.Group>
-          
-              <Button variant="warning" type="submit">
-                ADD A PET FOR ADOPTION
-              </Button>
-            </Form>
-          </div>
-  
+        <Form.Group className="mb-3">
+          <Form.Check
+            type="checkbox"
+            label="Check if the pet is neutered/sprayed."
+            name="neutered"
+          />
+        </Form.Group>
+
+        <Button variant="warning" type="submit">
+          ADD A PET FOR ADOPTION
+        </Button>
+      </Form>
+    </div>
   );
 }
 
