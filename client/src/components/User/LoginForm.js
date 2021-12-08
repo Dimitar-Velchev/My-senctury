ac2bc8fe55d23d3bf5bc914ec4bb66f43f4d130d
Dimitar-Velchev <77343528+Dimitar-Velchev@users.noreply.github.com>
2021-12-08 19:30:57 +0000

    login user

--- client/src/components/User/LoginForm.js
+++ client/src/components/User/LoginForm.js
@@ -26,12 +26,12 @@ function LoginForm({history}) {
         />
         <Form.Group className="mb-3">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email"  />
+          <Form.Control name="email" type="email" placeholder="Enter email"  />
         </Form.Group>
 
         <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" autoComplete="off" />
+          <Form.Control name="password" type="password" placeholder="Password" autoComplete="off" />
         </Form.Group>
 
         <Button variant="warning btn-primary" type="submit">
