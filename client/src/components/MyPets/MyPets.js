b554113fec85adc43b6a7aeef171a461cae896bd
Dimitar <dimitarvelchev59@gmail.com>
2021-12-11 17:37:56 +0200

    add profile page html/css

--- client/src/components/MyPets/MyPets.js
+++ client/src/components/MyPets/MyPets.js
@@ -1,7 +1,33 @@
-function MyPets (){
-    return (
-        <h1> My Pets </h1>
-    )
+import "./MyPets.css";
+import { NavLink } from "react-router-dom";
+
+function MyPets() {
+  return (
+    <section className="about">
+      <div className="main">
+        <img
+          src="./images/profilePic.jpg"
+          alt="profile pic"
+          title="Click to upload a profile pic"
+        />
+        <div className="about-text">
+          <h1>About</h1>
+          <h5>
+            Username: <span> Mitko </span>
+          </h5>
+          <h5>
+            E-mail: <span> dimityr_velchev@abv.bg </span>
+          </h5>
+          <h5>
+            Number of listings:<span> 2 </span>
+          </h5>
+          <h5>
+            View listings: <span>Masha, Molly </span>
+          </h5>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default MyPets
+export default MyPets;
